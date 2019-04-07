404a387fde6269870cc6b92800e1f145b441df04
Y123Y <1808487985@qq.com>
2019-04-07 20:47:03 +0800

    base project

---
+++ src/main/java/com/ctgu/gz/icms/IcmsApplication.java
@@ -0,0 +1,13 @@
+package com.ctgu.gz.icms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcmsApplication.class, args);
+    }
+
+}
